6277c75ec3fa953eef68785193a478d476cb6813
selviaLee <162958683+selviaLee@users.noreply.github.com>
2026-04-04 16:57:48 +0900

    Update landing.js

--- GamePartShare/landing.js
+++ GamePartShare/landing.js
@@ -1,1 +1,8 @@
 
+document.getElementById("goCreate").addEventListener("click", () => {
+  location.href = "nickname.html?mode=create";
+});
+
+document.getElementById("goJoin").addEventListener("click", () => {
+  location.href = "nickname.html?mode=join";
+});
